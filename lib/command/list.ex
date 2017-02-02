122600a87a87bf207d461e89e2400379e2e31169
faroreJP <creator.farore@gmail.com>
2017-02-02 23:16:57 +0900

    listは--directory表示を標準にした

--- lib/command/list.ex
+++ lib/command/list.ex
@@ -4,7 +4,6 @@ defmodule Chikae.Command.List do
 
   defmacro __using__(:parser) do
     quote do
-      defp parse_argument(opt,  :list,  "--directory",_), do: Map.put(opt, :type,     :directory)
       defp parse_argument(opt,  :list,  "--raw", _),      do: Map.put(opt, :raw,      true)
       defp parse_argument(opt,  :list,  "--verbose", _),  do: Map.put(opt, :verbose,  true)
       defp parse_argument(opt,  :list,  "--all", _),      do: Map.put(opt, :all,      true)
@@ -14,6 +13,12 @@ defmodule Chikae.Command.List do
   defmacro __using__(:executioner) do
     quote do
       def execute(:list, opt) do
+
+        opt = case Map.fetch(opt, :type) do
+          {:ok, _} -> opt
+          :error -> Map.put(opt, :type, :directory)
+        end
+
         Repository.get_all()
         |> Enum.filter( fn(x) -> is_all(opt) or !x.is_pruned end )
         |> List.foldl("", fn(x, acc) -> "#{acc}#{Task.to_s(x, opt)}\r\n" end)
